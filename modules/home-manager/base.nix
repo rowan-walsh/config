d1501729b546360ffb401cb7901ab105d4138a37
Rowan Walsh <1158758+rowan-walsh@users.noreply.github.com>
2024-08-22 20:34:39 -0700

    VScode settings

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -16,9 +16,35 @@
   programs = {
     git = {
       enable = true;
+      userName = "Rowan Walsh";
+      userEmail = "1158758+rowan-walsh@users.noreply.github.com";
+      signing.signByDefault = true;
+      signing.key = "7C222EAA5A246E8F";
+      extraConfig = {
+        init.defaultBranch = "main";
+        core.autocrlf = "input";
+      };
     };
     bash = {
       enable = true;
+    };
+    jujutsu = {
+      enable = true;
+      settings = {
+        "user.name" = "Rowan Walsh";
+        "user.email" = "1158758+rowan-walsh@users.noreply.github.com";
+
+        "ui.default-command" = "status";
+        "ui.pager" = "less -FRX";
+        "ui.editor" = "code --wait";
+        "ui.diff-editor" = ":builtin";
+        "ui.merge-editor" = ''["code", "--wait", "--merge", "$left", "$right", "$base", "$output"]'';
+        "ui.merge-tools.code.merge-tool-edits-conflict-markers" = true;
+
+        "signing.sign-all" = true;
+        "signing.backend" = "gpg";
+        "signing.key" = "7C222EAA5A246E8F";
+      };
     };
   };
 
--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -1,6 +1,55 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   programs.brave.enable = true;
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+    enableExtensionUpdateCheck = false;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      charliermarsh.ruff
+      elixir-lsp.vscode-elixir-ls
+      equinusocio.vsc-material-theme
+      equinusocio.vsc-material-theme-icons
+      github.copilot
+      github.copilot-chat
+      golang.go
+      ms-azuretools.vscode-docker
+      ms-python.debugpy
+      ms-python.python
+      ms-python.vscode-pylance
+      ms-toolsai.jupyter
+      ms-toolsai.jupyter-keymap
+      ms-toolsai.jupyter-renderers
+      ms-toolsai.vscode-jupyter-cell-tags
+      ms-toolsai.vscode-jupyter-slideshow
+      ms-vscode-remote.remote-containers
+      ms-vscode-remote.remote-ssh
+      ms-vscode-remote.remote-ssh-edit
+      ms-vscode-remote.remote-wsl
+      ms-vscode.cpptools
+      ms-vscode.hexeditor
+      ms-vscode.makefile-tools
+      ms-vscode.powershell
+      nefrob.vscode-just-syntax
+      pkief.material-icon-theme
+      redhat.vscode-xml
+      rust-lang.rust-analyzer
+      tamasfe.even-better-toml
+      vadimcn.vscode-lldb
+      ziglang.vscode-zig
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "alignment";
+        publisher = "annsk";
+        version = "0.3.0";
+        sha256 = "da29a516efb6dcdff716eb8150a664a5102f6be4ee95cc463f65d5f41d5933b0";
+      }
+    ];
+    userSettings = lib.importJSON ./vscode/settings.json;
+  };
 
   dconf = {
     enable = true;
@@ -17,23 +66,23 @@
         color-scheme = "prefer-dark";
       };
       # inspo: https://github.com/NixOS/nixpkgs/issues/114514
-      "org/gnome/mutter" = {
-        # Fractional scaling
-        experimental-features = ["scale-monitor-framebuffer"];
-      };
+      # "org/gnome/mutter" = {
+      #   # Fractional scaling
+      #   experimental-features = ["scale-monitor-framebuffer"];
+      # };
       "org/gnome/settings-daemon/plugins/color" = {
         night-light-enabled = true;
         night-light-temperature = 3700;
       };
-      #   "org/gnome/desktop/background" = {
-      #       picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/morphogenesis-l.svg";
-      #       picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/morphogenesis-d.svg";
-      #   };
-      #   "org/gnome/desktop/screensaver" = {
-      #       picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/morphogenesis-d.svg";
-      #       primary-color = "#e18477";
-      #       secondary-color = "#000000";
-      #   };
+      "org/gnome/desktop/background" = {
+        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/morphogenesis-l.svg";
+        picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/morphogenesis-d.svg";
+      };
+      "org/gnome/desktop/screensaver" = {
+        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/morphogenesis-d.svg";
+        primary-color = "#e18477";
+        secondary-color = "#000000";
+      };
       "org/gnome/shell" = {
         disable-user-extensions = false;
 
