73de7c63f552c93779fd213404e1e3a2a9eb4254
Rowan Walsh <1158758+rowan-walsh@users.noreply.github.com>
2024-08-26 22:31:51 -0700

    Fix formatting of vscode extensions
    Also add editorconfig extension

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -7,48 +7,50 @@
   programs.vscode = {
     enable = true;
     enableExtensionUpdateCheck = false;
-    extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-      charliermarsh.ruff
-      elixir-lsp.vscode-elixir-ls
-      equinusocio.vsc-material-theme
-      equinusocio.vsc-material-theme-icons
-      github.copilot
-      github.copilot-chat
-      golang.go
-      ms-azuretools.vscode-docker
-      ms-python.debugpy
-      ms-python.python
-      ms-python.vscode-pylance
-      ms-toolsai.jupyter
-      ms-toolsai.jupyter-keymap
-      ms-toolsai.jupyter-renderers
-      ms-toolsai.vscode-jupyter-cell-tags
-      ms-toolsai.vscode-jupyter-slideshow
-      ms-vscode-remote.remote-containers
-      ms-vscode-remote.remote-ssh
-      ms-vscode-remote.remote-ssh-edit
-      ms-vscode-remote.remote-wsl
-      ms-vscode.cpptools
-      ms-vscode.hexeditor
-      ms-vscode.makefile-tools
-      ms-vscode.powershell
-      nefrob.vscode-just-syntax
-      pkief.material-icon-theme
-      redhat.vscode-xml
-      rust-lang.rust-analyzer
-      tamasfe.even-better-toml
-      vadimcn.vscode-lldb
-      ziglang.vscode-zig
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "alignment";
-        publisher = "annsk";
-        version = "0.3.0";
-        sha256 = "da29a516efb6dcdff716eb8150a664a5102f6be4ee95cc463f65d5f41d5933b0";
-      }
-    ];
+    extensions = with pkgs.vscode-extensions;
+      [
+        bbenoist.nix
+        charliermarsh.ruff
+        elixir-lsp.vscode-elixir-ls
         editorconfig.editorconfig
+        equinusocio.vsc-material-theme
+        equinusocio.vsc-material-theme-icons
+        github.copilot
+        github.copilot-chat
+        golang.go
+        ms-azuretools.vscode-docker
+        ms-python.debugpy
+        ms-python.python
+        ms-python.vscode-pylance
+        ms-toolsai.jupyter
+        ms-toolsai.jupyter-keymap
+        ms-toolsai.jupyter-renderers
+        ms-toolsai.vscode-jupyter-cell-tags
+        ms-toolsai.vscode-jupyter-slideshow
+        ms-vscode-remote.remote-containers
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit
+        ms-vscode-remote.remote-wsl
+        ms-vscode.cpptools
+        ms-vscode.hexeditor
+        ms-vscode.makefile-tools
+        ms-vscode.powershell
+        nefrob.vscode-just-syntax
+        pkief.material-icon-theme
+        redhat.vscode-xml
+        rust-lang.rust-analyzer
+        tamasfe.even-better-toml
+        vadimcn.vscode-lldb
+        ziglang.vscode-zig
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "alignment";
+          publisher = "annsk";
+          version = "0.3.0";
+          sha256 = "da29a516efb6dcdff716eb8150a664a5102f6be4ee95cc463f65d5f41d5933b0";
+        }
+      ];
     userSettings = lib.importJSON ./vscode/settings.json;
   };
 
