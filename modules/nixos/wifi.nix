4f4fcf2d677c6f29cac4e67af3f1992611c75150
Rowan Walsh <1158758+rowan-walsh@users.noreply.github.com>
2024-08-27 21:00:53 -0700

    Use NetworkManager for wifi

--- modules/nixos/wifi.nix
+++ modules/nixos/wifi.nix
@@ -1,25 +1,17 @@
 {config, ...}: {
-  sops.secrets."wireless-env" = {};
-
-  networking.networkmanager.unmanaged = [
-    "*"
-    "except:type:wwan"
-    "except:type:gsm"
-  ];
-
-  networking.wireless = {
-    enable = true;
-    environmentFile = config.sops.secrets."wireless-env".path;
-    networks = {
-      "@home_uuid@" = {
-        psk = "@home_psk@";
-      };
-    };
+  sops.secrets."wifi_1.nmconnection.ini" = {
+    sopsFile = ./../../secrets/wifi_1.nmconnection.ini;
+    format = "ini";
+    path = "/etc/NetworkManager/system-connections/wifi_1.nmconnection";
+    mode = "0600";
+    reloadUnits = ["network-manager.service"];
   };
 
-  environment.persistence."/persist" = {
-    files = [
-      "/etc/wpa_supplicant.conf"
-    ];
+  sops.secrets."wifi_2.nmconnection.ini" = {
+    sopsFile = ./../../secrets/wifi_2.nmconnection.ini;
+    format = "ini";
+    path = "/etc/NetworkManager/system-connections/wifi_2.nmconnection";
+    mode = "0600";
+    reloadUnits = ["network-manager.service"];
   };
 }
