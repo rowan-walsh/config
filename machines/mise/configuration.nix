4d85d11d7807f5a952792f6148c1992728582031
Rowan Walsh <1158758+rowan-walsh@users.noreply.github.com>
2024-10-02 21:48:23 -0700

    Give mise a fixed IP address

--- machines/mise/configuration.nix
+++ machines/mise/configuration.nix
@@ -23,6 +23,14 @@
   boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = false;
 
+  # Static IP address since this is the DNS and DHCP server
+  networking.interfaces."end0".ipv4.addresses = [
+    {
+      address = "192.168.1.2";
+      prefixLength = 24;
+    }
+  ];
+
   home-manager = {
     extraSpecialArgs = {inherit inputs outputs;};
     useGlobalPkgs = true;
